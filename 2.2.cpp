dfcedf0700934b653cde6a2e264dabb060579456
ru0505 <127956898+ru0505@users.noreply.github.com>
2023-04-26 07:46:22 -0500

    Add files via upload

---
+++ 2.2.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+void ret(int y[], int x) {
+    int a=0;
+    for (int i=0; i<x ; i++) {
+        a = y[i];        
+        int u = y[x-i]; 
+        int x= y[u];
+        y[i]=x;
+        y[u]=a;
+    }
+    cout<<y[a]<<end;
+}
+
+
+int main() {
+    int a[] = { 1, 2, 3, 2, 1 };
+    int tam = sizeof(a);
+    ret(a,tam);
+    return 0;
+} 
