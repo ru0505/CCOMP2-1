615803498fca58401714fe5e53664ac26add3b5b
ru0505 <127956898+ru0505@users.noreply.github.com>
2023-05-18 08:25:11 -0500

    Add files via upload

---
+++ time/time.h
@@ -0,0 +1,15 @@
+#ifndef TIME_H
+#define TIME_H
+
+class Time {
+public:
+void setTime(int, int, int);
+unsigned int getHour() const;
+
+private:
+unsigned int hour{0};
+unsigned int minute{0};
+ unsigned int second{0};
+};
+
+#endif
