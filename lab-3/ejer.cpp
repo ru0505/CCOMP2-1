36428a5a47a4a459dd7664b10520217540deecbb
ru0505 <127956898+ru0505@users.noreply.github.com>
2023-09-18 08:40:49 -0500

    Update ejer.cpp

--- lab-3/ejer.cpp
+++ lab-3/ejer.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a;
-    cout<<"escriba un numero: ";
-    cin>>a;
-    if (a != 1 && a!=0){
-        for (int  i =2;i <=a;i++){
-            if(a%i ==0){
-                
-                continue;
-           } else{
-                cout << i << " ";
-                }
-            
+int main() {
+    int n;
+    cout << "Ingrese un numero: ";
+    cin >> n;
+
+    for (int i = 2; i <= n; i++) {
+        int x{ 0 };
+        if (i <= 1) {
+            continue;
+        }
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                x = 1;
+                break;
+            }
+        }
+        if (x == 0) {
+            cout << i << " ";
         }
     }
-    
-    return 0;
-
-} 
-
+    return 0;
+}
