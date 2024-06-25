0c2969693c2c795c0ddb7c9b21ad864b6088fb21
ru0505 <127956898+ru0505@users.noreply.github.com>
2024-06-25 17:59:46 -0500

    Add files via upload

---
+++ desa/Formulario/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Formulario_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void btn_Listo_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new curriculum(nombre.Text, apellido.Text, Estudios.Text, xp.Text, area.Text, tel.Text, correo.Text));
+          
+        }
+    }
+}
---
+++ desa/Formulario/curriculum.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formulario_1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class curriculum : ContentPage
+    {
+        public curriculum(string nombre, string apellido, string estudios, string años, string area, string numero, string correo)
+        {
+            InitializeComponent();
+
+            n.Text = n.Text+nombre;
+            a.Text = a.Text+apellido;
+            es.Text = es.Text+estudios;
+            ar.Text = ar.Text+area;
+            xp.Text = "Tengo " + años + " años de experiencia laboral";
+            tel.Text = tel.Text+numero;
+            co.Text = co.Text+correo;
+        }
+    }
+}
