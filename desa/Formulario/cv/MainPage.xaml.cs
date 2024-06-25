70e3d2e9b1a634f44839db5eb22297896ecd36b8
ru0505 <127956898+ru0505@users.noreply.github.com>
2024-06-25 18:50:09 -0500

    Add files via upload

---
+++ desa/Formulario/cv/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MiCVApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+           
+            List<string> habilidadesBlandas = new List<string>
+            {
+                "Comunicación efectiva",
+                "Trabajo en equipo",
+                "Adaptabilidad",
+                "Resolución de problemas",
+                "Liderazgo"
+            };
+
+            
+            HabilidadesBlandasList.ItemsSource = habilidadesBlandas;
+        }
+    }
+}
